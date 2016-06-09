99a9d17338189f7f49ebc58e76aeb026fd9050dd
Youssouf Souare <yssouae@gmail.com>
2016-06-09 14:38:24 +0200

    Add a constructor from super class Account !!

--- src/com/yss/CreditAccount.java
+++ src/com/yss/CreditAccount.java
@@ -3,5 +3,8 @@ package com.yss;
 /**
  * Created by yss on 09-06-2016.
  */
-public class CreditAccount {
+public class CreditAccount extends Account {
+    public CreditAccount(String name, String amount) {
+        super(name, amount);
+    }
 }
